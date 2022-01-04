9aa56b021b7ce8dba68da36f947b910211cf1097
TristanBlackwell <tristanblackwell1@gmail.com>
2022-01-04 15:05:33 +0000

    Add plaid transaction retrieval logic

--- expense-app/lambdas/src/checkWeeksExpense.ts
+++ expense-app/lambdas/src/checkWeeksExpense.ts
@@ -1,11 +1,89 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import {
+  Configuration,
+  PlaidApi,
+  PlaidEnvironments,
+  Transaction,
+  TransactionsGetRequest,
+} from "plaid";
+
+const { PLAID_CLIENT_ID, PLAID_SECRET } = process.env;
 
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const queries = JSON.stringify(event.queryStringParameters);
-  return {
-    statusCode: 200,
-    body: `Queries: ${queries}`,
+  // const queries = JSON.stringify(event.queryStringParameters);
+  // return {
+  //   statusCode: 200,
+  //   body: `Queries: ${queries}`,
+  // };
+
+  const configuration = new Configuration({
+    basePath: PlaidEnvironments.sandbox,
+    baseOptions: {
+      Headers: {
+        "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+        "PLAID-SECRET": PLAID_SECRET,
+      },
+    },
+  });
+  const plaidClient = new PlaidApi(configuration);
+
+  const today = new Date();
+  const oneWeekAgo = new Date(new Date().setDate(today.getDate() - 7));
+
+  // Our dates need to be in string format YYYY-MM-DD for Plaid request.
+  const todayString = `${today.getFullYear().toString()}-${today
+    .getMonth()
+    .toString()}-${today.getDate().toString()}`;
+  const oneWeekAgoString = `${oneWeekAgo.getFullYear().toString()}-${oneWeekAgo
+    .getMonth()
+    .toString()}-${oneWeekAgo.getDate().toString()}`;
+
+  const request: TransactionsGetRequest = {
+    access_token: "",
+    start_date: oneWeekAgoString,
+    end_date: todayString,
   };
+
+  const transactions: Transaction[] = [];
+  try {
+    const transactionResponse = await plaidClient.transactionsGet(request);
+
+    transactions.concat(transactionResponse.data.transactions);
+    const totalTransactions = transactionResponse.data.total_transactions;
+
+    // If we couldn't get all the transaction items on the first request
+    // lets paginate our request.
+    const moreTransactionRequests = [];
+    while (transactions.length < totalTransactions) {
+      const paginatedRequest: TransactionsGetRequest = {
+        ...request,
+        options: {
+          offset: transactions.length,
+        },
+      };
+
+      moreTransactionRequests.push(
+        plaidClient.transactionsGet(paginatedRequest)
+      );
+    }
+
+    const moreTransactions = await Promise.all(moreTransactionRequests);
+
+    moreTransactions.forEach((mtxs) => {
+      transactions.concat(mtxs.data.transactions);
+    });
+  } catch (err) {
+    console.error("Plaid transactions request failed!");
+  }
+
+  let totalSpend = 0;
+  transactions.forEach((transaction) => {
+    // Sum up all debit transactions, we only care about outgoings
+    // so ignore - values.
+    if (transaction.amount > 0) totalSpend += transaction.amount;
+  });
+
+  // Get target value and compare.
 };
